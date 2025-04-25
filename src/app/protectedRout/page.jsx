1a6a9cc5754ebfcaa32c143ec01a08f04070ab85
yiconic <rp5865442@gmail.com>
2025-04-26 02:37:32 +0530

    add routing from clien side

---
+++ src/app/protectedRout/page.jsx
@@ -0,0 +1,24 @@
+'use client'
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react'
+
+const Page = () => {
+
+    const router=useRouter();
+
+    useEffect(()=>{
+         const token=localStorage.getItem("token")
+         if(!token){
+            router.push("/login")
+         }
+    },[])
+
+
+  return (
+    <div>
+      this is  a protected rout .
+    </div>
+  )
+}
+
+export default Page
